e6cd74cdb5340e39a0783431107ce39815d5b1c7
ahmed samy <ahmbdelkhalek@gmail.com>
2025-04-28 19:03:35 +0300

    feat: add implementation of registration function

--- src/main/kotlin/data/dataSource/DataSourceImpl.kt
+++ src/main/kotlin/data/dataSource/DataSourceImpl.kt
@@ -1,29 +1,36 @@
 package data.dataSource
 
 import data.database.UsersCsvParser
-import data.dto.LogEntityDto
-import data.dto.ProjectDto
-import data.dto.TaskDto
-import data.dto.UserDto
+import data.dto.*
+import java.util.*
 
 class DataSourceImpl(
-    private val usersCsvParser: UsersCsvParser
-
-
-): DataSource {
-    override fun getAllUsers(): List<UserDto> {
-        TODO("Not yet implemented")
-    }
-
-    override fun getAllProjects(): List<ProjectDto> {
-        TODO("Not yet implemented")
-    }
-
-    override fun getAllTasks(): List<TaskDto> {
-        TODO("Not yet implemented")
-    }
-
-    override fun getAllAuditRecords(): List<LogEntityDto> {
-        TODO("Not yet implemented")
-    }
+	private val usersCsvParser: UsersCsvParser
+) : DataSource {
+	override fun createUser(name: String, password: String, userType: UserType) {
+		val userDto = UserDto(
+			id = UUID.randomUUID(),
+			name = name,
+			password = password,
+			type = userType
+		)
+		
+		usersCsvParser.write(userDto)
+	}
+	
+	override fun getAllUsers(): List<UserDto> {
+		TODO("Not yet implemented")
+	}
+	
+	override fun getAllProjects(): List<ProjectDto> {
+		TODO("Not yet implemented")
+	}
+	
+	override fun getAllTasks(): List<TaskDto> {
+		TODO("Not yet implemented")
+	}
+	
+	override fun getAllAuditRecords(): List<LogEntityDto> {
+		TODO("Not yet implemented")
+	}
 }
