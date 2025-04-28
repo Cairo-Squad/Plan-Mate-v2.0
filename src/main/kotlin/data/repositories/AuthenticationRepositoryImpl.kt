e594a56acf2d36524c70dd6f2ec27365e2cdeac3
ahmed samy <ahmbdelkhalek@gmail.com>
2025-04-28 19:01:17 +0300

    feat: add the implementation of registration

--- src/main/kotlin/data/repositories/AuthenticationRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/AuthenticationRepositoryImpl.kt
@@ -1,10 +1,13 @@
 package data.repositories
 
 import data.dataSource.DataSource
+import data.dto.UserType
 import logic.repositories.AuthenticationRepository
 
-class AuthenticationRepositoryImpl (
-    private val dataSource: DataSource
-): AuthenticationRepository {
-
+class AuthenticationRepositoryImpl(
+	private val dataSource: DataSource
+) : AuthenticationRepository {
+	override fun createUser(name: String, password: String, userType: UserType) {
+		dataSource.createUser(name, password, userType)
+	}
 }
