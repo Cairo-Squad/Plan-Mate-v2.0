3fa8128393a2a8f8e6ee4b93b43375936680b47c
ahmed samy <ahmbdelkhalek@gmail.com>
2025-04-28 19:04:12 +0300

    feat: add the logic of writing on the file

--- src/main/kotlin/data/database/UsersCsvParser.kt
+++ src/main/kotlin/data/database/UsersCsvParser.kt
@@ -1,11 +1,29 @@
 package data.database
 
-class UsersCsvParser: Parser {
-    override fun <T> write(entity: T) {
-        TODO("Not yet implemented")
-    }
+import data.dto.UserDto
+import data.util.Constants
+import java.io.File
+import java.io.FileWriter
 
-    override fun <T> read(): T {
-        TODO("Not yet implemented")
-    }
+class UsersCsvParser : Parser {
+	override fun <T> write(entity: T) {
+		val userDto = entity as UserDto
+		
+		val file = File(Constants.USERS_CSV_FILE_PATH)
+		val isNewFile = !file.exists()
+		
+		FileWriter(file, true).use { writer ->
+			if (isNewFile) {
+				writer.write("id,name,password,type\n")
+			}
+			
+			
+			writer.append("${userDto.id},${userDto.name},${userDto.password},${userDto.type}\n")
+			writer.flush()
+		}
+	}
+	
+	override fun <T> read(): T {
+		TODO("Not yet implemented")
+	}
 }
