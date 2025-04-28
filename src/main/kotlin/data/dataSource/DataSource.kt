1c5cb826a1b990c2a91da669e53f45567e39f3b5
ahmed samy <ahmbdelkhalek@gmail.com>
2025-04-28 19:02:55 +0300

    feat: add registration function

--- src/main/kotlin/data/dataSource/DataSource.kt
+++ src/main/kotlin/data/dataSource/DataSource.kt
@@ -1,11 +1,9 @@
 package data.dataSource
 
-import data.dto.UserDto
-import data.dto.ProjectDto
-import data.dto.TaskDto
-import data.dto.LogEntityDto
+import data.dto.*
 
 interface DataSource {
+    fun createUser(name:String, password: String, userType: UserType)
     fun getAllUsers(): List<UserDto>
     fun getAllProjects(): List<ProjectDto>
     fun getAllTasks(): List<TaskDto>
