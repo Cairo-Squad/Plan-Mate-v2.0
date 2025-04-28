e12cfe07d71abd4d6fb9574f0d657f8fefe306bf
Eman Abobakr <eman.abobakr@vodafone.com>
2025-04-28 23:58:35 +0300

    Revert "Revert "Revert "test failed test case"""
    This reverts commit 51ff78d5ad1039d6bfe191f6baed2a9be33d9f65.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,3 @@ package org.example
 fun main() {
     println("Hello World!")
 }
-
-fun sum(num1: Int, num2: Int) = num1 + num2
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -1,17 +1,9 @@
 import org.example.main
-import org.example.sum
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
 
 class MainKtTest {
     @Test
     fun test_main() {
         main()
-    }
-
-    @Test
-    fun test_sum() {
-        val result = sum(3,5)
-        assertEquals(4, result)
     }
 }
