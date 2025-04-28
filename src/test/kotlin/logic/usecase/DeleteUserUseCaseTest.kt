d60f14cfd0467b246cf1459d90a734f042b2bc5e
norhanadel367 <norhanadel367@gmail.com>
2025-04-28 18:59:21 +0300

    feat: create DeleteUserUseCase ,add unit testing for delete user useCase

---
+++ src/test/kotlin/logic/usecase/DeleteUserUseCaseTest.kt
@@ -0,0 +1,50 @@
+package logic.usecase
+
+import com.google.common.truth.Truth.assertThat
+import io.mockk.every
+import io.mockk.mockk
+import logic.repositories.AuthenticationRepository
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.*
+
+class DeleteUserUseCaseTest {
+    private lateinit var authenticationRepository : AuthenticationRepository
+    private lateinit var deleteUserUseCase : DeleteUserUseCase
+
+    @BeforeEach
+    fun setUp() {
+        authenticationRepository = mockk(relaxed = true)
+        deleteUserUseCase = DeleteUserUseCase(authenticationRepository)
+    }
+
+    @Test
+    fun `should return true when userid exist`() {
+        // Given
+        val existingUserId = UUID.randomUUID()
+
+        // Use FakeData
+        every { authenticationRepository.getUser() } returns FakeData.mockUsers
+
+        // When
+        val result = deleteUserUseCase.deleteUser(existingUserId)
+
+        // Then
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `should return false when userid not exist`() {
+        // Given
+        val notExistUserId = UUID.randomUUID()
+
+        // Use FakeData
+        every { authenticationRepository.getUser() } returns FakeData.mockUsers
+
+        // When
+        val result = deleteUserUseCase.deleteUser(notExistUserId)
+
+        // Then
+        assertThat(result).isFalse()
+    }
+}
