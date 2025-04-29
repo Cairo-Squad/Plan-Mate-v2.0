50f74b7554f676890f5b08710a229d109e441007
norhanadel367 <norhanadel367@gmail.com>
2025-04-29 19:02:15 +0300

    feat:  add ovrride function

--- src/main/kotlin/data/repositories/AuthenticationRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/AuthenticationRepositoryImpl.kt
@@ -1,10 +1,14 @@
 package data.repositories
 
 import data.dataSource.DataSource
+import data.dto.UserDto
 import logic.repositories.AuthenticationRepository
 
 class AuthenticationRepositoryImpl(
     private val dataSource: DataSource
 ) : AuthenticationRepository {
+    override fun getUser() : List<UserDto> {
+        TODO("Not yet implemented")
+    }
 
 }
