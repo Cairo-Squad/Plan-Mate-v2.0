a79535a209088f5a215686d2924d90964ccff0ed
Mahmoud Khairy <mahmoud5airy2000@gmail.com>
2025-04-29 20:27:49 +0300

    added testcases for editProjectDescriptionUseCase

---
+++ src/test/kotlin/EditProjectDescriptionTest.kt
@@ -0,0 +1,44 @@
+import io.mockk.every
+import io.mockk.mockk
+import logic.repositories.ProjectsRepository
+import logic.usecase.EditProjectDescriptionUseCase
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.UUID
+import kotlin.test.assertEquals
+
+class EditProjectDescriptionTest {
+
+    private val projectsRepository = mockk<ProjectsRepository>()
+    private lateinit var editProjectDescription: EditProjectDescriptionUseCase
+
+    @BeforeEach
+    fun setup() {
+        editProjectDescription = EditProjectDescriptionUseCase(projectsRepository)
+    }
+
+    @Test
+    fun `Given newDescription and project id,When updating database,Then returns success result`() {
+        //Given
+        val newDescription = "This is new Description"
+        val prjectID = UUID.randomUUID()
+        //When
+        val result = editProjectDescription.editProjectDescription(newDescription, prjectID)
+        //Then
+        assertEquals(expected = Result.success(Unit), actual = result)
+    }
+
+    @Test
+    fun `Given newDescription and project id, When updating database, Then returns failure with cause`() {
+        //Given
+        val newDescription = "This is new Description"
+        val prjectID = UUID.randomUUID()
+        every { projectsRepository.editProjectDescription(newDescription, prjectID) } throws Exception()
+        //When
+        val result = editProjectDescription.editProjectDescription(newDescription, prjectID)
+        //Then
+        assertEquals(expected = Result.failure(Exception()), actual = result)
+    }
+
+
+}
