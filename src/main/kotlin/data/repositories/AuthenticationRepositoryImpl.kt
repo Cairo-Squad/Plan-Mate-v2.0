04ec7c2aaf431cd8045cd49459d806f42afbb010
ahmed samy <ahmbdelkhalek@gmail.com>
2025-04-29 18:13:37 +0300

    refactor: add override function

--- src/main/kotlin/data/repositories/AuthenticationRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/AuthenticationRepositoryImpl.kt
@@ -1,10 +1,14 @@
 package data.repositories
 
 import data.dataSource.DataSource
+import data.dto.UserType
 import logic.repositories.AuthenticationRepository
 
 class AuthenticationRepositoryImpl(
     private val dataSource: DataSource
 ) : AuthenticationRepository {
-
+    override fun createUser(name: String, password: String, userType: UserType) {
+        TODO("Not yet implemented")
+    }
+    
 }
