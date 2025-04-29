e89890aaa63149ce5d9658ca212e014ce79e7332
Mahmoud Khairy <mahmoud5airy2000@gmail.com>
2025-04-29 23:28:13 +0300

    implemented logic for edit project title and description useCases

--- src/main/kotlin/logic/usecase/EditProjectDescriptionUseCase.kt
+++ src/main/kotlin/logic/usecase/EditProjectDescriptionUseCase.kt
@@ -5,7 +5,12 @@ import java.util.UUID
 
 class EditProjectDescriptionUseCase(private val projectsRepository: ProjectsRepository) {
 
-    fun editProjectDescription(newDescription: String, projectID: UUID):Result<Unit>{
-        TODO()
+    fun editProjectDescription(newDescription: String, projectID: UUID): Result<Unit> {
+        return try {
+            projectsRepository.editProjectDescription(newDescription, projectID)
+            Result.success(Unit)
+        } catch (exception: Exception) {
+            Result.failure(exception)
+        }
     }
 }
--- src/main/kotlin/logic/usecase/EditProjectTitleUseCase.kt
+++ src/main/kotlin/logic/usecase/EditProjectTitleUseCase.kt
@@ -1,11 +1,18 @@
 package logic.usecase
 
 import logic.repositories.ProjectsRepository
+import java.io.IOException
 import java.util.*
 
 class EditProjectTitleUseCase(private val projectsRepository: ProjectsRepository) {
 
     fun editProjectTitle(projectId: UUID, newTitle: String): Result<Unit> {
-        TODO()
+        return try {
+            require(newTitle.isNotBlank()) { "Title can't be blank" }
+            projectsRepository.editProjectTitle(projectId = projectId, newTitle = newTitle)
+            Result.success(Unit)
+        } catch (exception: Exception) {
+            Result.failure(exception)
+        }
     }
 }
