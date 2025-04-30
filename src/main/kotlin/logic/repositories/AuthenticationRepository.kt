93d83ba7b44d3251f7dfad43b3bdd0eac9fc7872
norhanadel367 <92339414+norhanadel367@users.noreply.github.com>
2025-04-30 13:46:23 +0300

    Update AuthenticationRepository.kt

--- src/main/kotlin/logic/repositories/AuthenticationRepository.kt
+++ src/main/kotlin/logic/repositories/AuthenticationRepository.kt
@@ -6,7 +6,7 @@ import java.util.*
 
 interface AuthenticationRepository {
     fun createUser(name : String, password : String, userType : UserType)
-    fun getAllUser() : List<User>
+    fun getAllUsers() : List<User>
     fun loginUser(name : String, password : String) : Boolean
     fun deleteUser(userId: UUID): Boolean
 
