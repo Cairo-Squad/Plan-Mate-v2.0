550440bd0f37e2deb66873aa464a549410300d2d
Mahmoud Khairy <mahmoud5airy2000@gmail.com>
2025-04-30 19:31:54 +0300

    initial commit for adding project log

---
+++ src/main/kotlin/logic/usecase/AddProjectLogUseCase.kt
@@ -0,0 +1,11 @@
+package logic.usecase
+
+import logic.model.Log
+import logic.repositories.LogsRepository
+
+class AddProjectLogUseCase (private val logsRepository: LogsRepository) {
+
+    fun addProjectLog(log: Log) {
+
+    }
+}
