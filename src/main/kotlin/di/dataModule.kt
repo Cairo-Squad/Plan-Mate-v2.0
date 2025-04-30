ed71b9b2eb26b8f095f396ffef4428c3dfb8707f
Mahmoud Khairy <mahmoud5airy2000@gmail.com>
2025-04-30 04:14:06 +0300

    added project csv file path and headers

--- src/main/kotlin/di/dataModule.kt
+++ src/main/kotlin/di/dataModule.kt
@@ -4,10 +4,12 @@ import data.dataSource.CsvDataSource
 import data.dataSource.DataSource
 import data.database.FileHandler
 import data.database.ProjectsCsvHandler
+import data.database.util.CsvConstants.PROJECTS_CSV_FILE_HEADERS
+import data.database.util.CsvConstants.PROJECTS_CSV_FILE_PATH
 import data.dto.ProjectDto
 import org.koin.dsl.module
 
 val dataModule = module {
-    single<FileHandler<ProjectDto>> { ProjectsCsvHandler(TODO(), TODO()) }
+    single<FileHandler<ProjectDto>> { ProjectsCsvHandler(PROJECTS_CSV_FILE_PATH, PROJECTS_CSV_FILE_HEADERS) }
     single<DataSource> { CsvDataSource(projectsCsvHandler = get()) }
 }
