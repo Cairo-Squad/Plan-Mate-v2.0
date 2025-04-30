96a1090a4f277d142da7a63257500bac87d42181
norhanadel367 <norhanadel367@gmail.com>
2025-04-30 12:15:22 +0300

    refactor:remove loginUser from AuthenticationRepository

--- src/main/kotlin/logic/repositories/AuthenticationRepository.kt
+++ src/main/kotlin/logic/repositories/AuthenticationRepository.kt
@@ -1,11 +1,10 @@
 package logic.repositories
 
-import data.dto.User
 import data.dto.UserType
+import logic.model.User
 
 interface AuthenticationRepository {
-	fun createUser(name: String, password:String, userType: UserType)
-  fun getAllUser() : List<User>
-  fun loginUser(name: String, password: String):Booleane
+    fun createUser(name : String, password : String, userType : UserType)
+    fun getAllUser() : List<User>
 
 }
