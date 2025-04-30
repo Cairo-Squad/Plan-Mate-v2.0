1d766f1ba7fce1fe713d41192a59c8c1c2be9165
norhanadel367 <norhanadel367@gmail.com>
2025-04-30 17:07:59 +0300

    refactor: edite formatting LoginUserUseCase

--- src/main/kotlin/logic/usecase/LoginUserUseCase.kt
+++ src/main/kotlin/logic/usecase/LoginUserUseCase.kt
@@ -4,10 +4,10 @@ import data.dto.UserType
 import logic.repositories.AuthenticationRepository
 
 class LoginUserUseCase(private val authenticationRepository : AuthenticationRepository) {
-    val users = authenticationRepository.getAllUser()
+    val users = authenticationRepository.getAllUsers()
 
     fun login(name : String, password : String) : Boolean {
-        val isFoundUser=users.any { it.name == name && it.password == password }
+        val isFoundUser = users.any { it.name == name && it.password == password }
         return isFoundUser
     }
 }
