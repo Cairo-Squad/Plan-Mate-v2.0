fc2aee21a56dd0e4bf4afba78f907cdd01e661dc
Abdo Sharaf <bodyasharaf8@gmail.com>
2025-04-30 14:31:27 +0300

    feat: add tasks logs repository methods

--- src/main/kotlin/data/repositories/LogsRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/LogsRepositoryImpl.kt
@@ -1,9 +1,18 @@
 package data.repositories
 
 import data.dataSource.DataSource
+import logic.model.Log
 import logic.repositories.LogsRepository
+import java.util.*
 
 class LogsRepositoryImpl(
     private val dataSource: DataSource
 ) : LogsRepository {
+    override fun createLog(log: Log) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getTaskLogs(taskId: UUID): List<Log> {
+        TODO("Not yet implemented")
+    }
 }
--- src/main/kotlin/logic/repositories/LogsRepository.kt
+++ src/main/kotlin/logic/repositories/LogsRepository.kt
@@ -1,4 +1,9 @@
 package logic.repositories
 
+import logic.model.Log
+import java.util.*
+
 interface LogsRepository {
+    fun createLog(log: Log)
+    fun getTaskLogs(taskId: UUID): List<Log>
 }
