a1ee5bf9606af2a7225934a44cc96637beabf015
Mahmoud Khairy <mahmoud5airy2000@gmail.com>
2025-04-30 23:39:02 +0300

    send log object to the logsRepository

--- src/main/kotlin/logic/usecase/AddProjectLogUseCase.kt
+++ src/main/kotlin/logic/usecase/AddProjectLogUseCase.kt
@@ -6,6 +6,6 @@ import logic.repositories.LogsRepository
 class AddProjectLogUseCase(private val logsRepository: LogsRepository) {
 
     fun addProjectLog(log: Log) {
-        logs
+        logsRepository.addProjectLog(log)
     }
 }
