6f52cf29b635dbeca45bd94d2f23f3c1a323cb1d
mohamed_Galal <az.galal666@gmail.com>
2025-05-01 19:42:11 +0300

    refactor: handle exception Handler

--- src/main/kotlin/data/database/CsvFileHandler.kt
+++ src/main/kotlin/data/database/CsvFileHandler.kt
@@ -27,18 +27,30 @@ abstract class CsvFileHandler<DTO>(
     }
 
     override fun write(entity: DTO) {
-        val newRow = fromDtoToCsvRow(entity)
-        BufferedWriter(FileWriter(file, true)).use { writer ->
-            writer.appendLine(newRow)
+        try {
+            val newRow = fromDtoToCsvRow(entity)
+            BufferedWriter(FileWriter(file, true)).use { writer ->
+                writer.appendLine(newRow)
+            }
+        } catch (e: IOException) {
+            throw CsvWriteException()
+        } catch (e: Exception) {
+            throw UnknownException()
         }
     }
 
     private fun writeAll(entities: List<DTO>) {
-        BufferedWriter(FileWriter(file, false)).use { writer ->
-            writer.appendLine(headers.joinToString(","))
-            entities.forEach { entity ->
-                writer.appendLine(fromDtoToCsvRow(entity))
+        try {
+            BufferedWriter(FileWriter(file, false)).use { writer ->
+                writer.appendLine(headers.joinToString(","))
+                entities.forEach { entity ->
+                    writer.appendLine(fromDtoToCsvRow(entity))
+                }
             }
+        } catch (e: IOException) {
+            throw CsvWriteException()
+        } catch (e: Exception) {
+            throw UnknownException()
         }
     }
 
