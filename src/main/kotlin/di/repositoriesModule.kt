6574bddfd225f7aa462c289e47bb71a376f98339
Ahmed Mahgoub <aed7862@gmail.com>
2025-05-01 19:49:13 +0300

    feat: add app, repository, and use cases modules for di

--- src/main/kotlin/di/repositoriesModule.kt
+++ src/main/kotlin/di/repositoriesModule.kt
@@ -4,7 +4,6 @@ import data.repositories.ProjectsRepositoryImpl
 import logic.repositories.ProjectsRepository
 import org.koin.dsl.module
 
-
 val repositoryModule = module {
     single<ProjectsRepository> { ProjectsRepositoryImpl(dataSource = get()) }
 }
--- src/main/kotlin/di/useCasesModule.kt
+++ src/main/kotlin/di/useCasesModule.kt
@@ -1,18 +1,13 @@
 package di
 
-import logic.usecase.project.AddProjectLogUseCase
+import logic.usecase.project.*
 import org.koin.dsl.module
-import logic.usecase.CreateProjectUseCase
-import logic.usecase.DeleteProjectUseCase
-import logic.usecase.EditProjectUseCase
-import logic.usecase.project.GetAllProjectsUseCase
-import logic.usecase.project.GetProjectByIdUseCase
 
 val useCasesModule = module {
+    single { CreateProjectUseCase(projectRepository = get()) }
     single { EditProjectUseCase(get()) }
-    single { AddProjectLogUseCase(get()) }
-    single { CreateProjectUseCase(repository = get()) }
-    single { GetProjectByIdUseCase(projectsRepository = get()) }
     single { DeleteProjectUseCase(projectsRepository = get()) }
+    single { GetProjectByIdUseCase(projectsRepository = get()) }
     single { GetAllProjectsUseCase(projectsRepository = get()) }
+    single { AddProjectLogUseCase(get()) }
 }
