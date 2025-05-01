7a17a45ed44db28037be90f9e8179ffa98c6e09e
Mahmoud Khairy <mahmoud5airy2000@gmail.com>
2025-05-01 19:02:24 +0300

    added test case for add project log use case

--- src/test/kotlin/logic/usecase/AddProjectLogUseCaseTest.kt
+++ src/test/kotlin/logic/usecase/AddProjectLogUseCaseTest.kt
@@ -2,11 +2,13 @@ package logic.usecase
 
 import data.dto.EntityType
 import data.dto.UserAction
+import io.mockk.every
 import io.mockk.mockk
 import logic.model.Log
 import logic.repositories.LogsRepository
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.time.LocalDateTime
 import java.util.*
 import kotlin.test.assertEquals
@@ -27,7 +29,18 @@ class AddProjectLogUseCaseTest {
         //When
         val result = addProjectLogUseCase.addProjectLog(projectLog)
         //Then
-        assertEquals(expected = Unit, actual = result)
+        assertEquals(expected = Result.success(Unit), actual = result)
+    }
+
+    @Test
+    fun `Given a project log,When adding to database,Then return failure`() {
+        //Given
+        every { logsRepository.addProjectLog(any()) } throws Exception()
+        val projectLog = createLog()
+        //When
+        val result = addProjectLogUseCase.addProjectLog(projectLog)
+        //Then
+        assertEquals(expected = Result.failure(Exception()), actual = result)
     }
 
     private fun createLog() = Log(
