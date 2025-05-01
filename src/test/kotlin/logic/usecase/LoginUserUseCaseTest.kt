81a35f39841623745bff73b294870f5e9f192bf9
norhanadel367 <norhanadel367@gmail.com>
2025-05-01 12:51:29 +0300

    refactor: edite formatting LoginUserUseCaseTest file

--- src/test/kotlin/logic/usecase/LoginUserUseCaseTest.kt
+++ src/test/kotlin/logic/usecase/LoginUserUseCaseTest.kt
@@ -21,11 +21,11 @@ class LoginUserUseCaseTest {
     @Test
     fun `should return true when valid username and password`() {
         //Given
-        val mockUsers=FakeData.mockUsers
+        val mockUsers = FakeData.mockUsers
         every { authenticationRepository.getAllUsers() } returns FakeData.mockUsers
 
         //when
-        val result = loginUserUseCase.login(mockUsers[0].name,mockUsers[0].password)
+        val result = loginUserUseCase.login(mockUsers[0].name, mockUsers[0].password)
 
         //Then
         assertThat(result).isTrue()
@@ -34,11 +34,11 @@ class LoginUserUseCaseTest {
     @Test
     fun `should return false when  username valid and password is invalid`() {
         //Given
-        val mockUsers=FakeData.mockUsers
+        val mockUsers = FakeData.mockUsers
         every { authenticationRepository.getAllUsers() } returns FakeData.mockUsers
 
         //when
-        val result = loginUserUseCase.login(mockUsers[0].name,"123456789")
+        val result = loginUserUseCase.login(mockUsers[0].name, "123456789")
 
         //Then
         assertThat(result).isFalse()
@@ -47,23 +47,27 @@ class LoginUserUseCaseTest {
     @Test
     fun `should return false when  username is invalid and password is valid`() {
         //Given
-        val mockUsers=FakeData.mockUsers
+        val mockUsers = FakeData.mockUsers
         every { authenticationRepository.getAllUsers() } returns FakeData.mockUsers
 
         //when
-        val result = loginUserUseCase.login("ali",mockUsers[0].password)
+        val result = loginUserUseCase.login("ali", mockUsers[0].password)
 
         //Then
         assertThat(result).isFalse()
     }
+
     @Test
     fun `should throw exception when repository throws error`() {
+        //Given
         every { authenticationRepository.getAllUsers() } throws RuntimeException("during get data error")
 
+        //when
         val exception = assertThrows<Exception> {
             loginUserUseCase.login("nour", "12345")
         }
 
+        //Then
         assertThat(exception).hasMessageThat().isEqualTo("error  during login")
     }
 }
