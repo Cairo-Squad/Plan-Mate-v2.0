e81f9b76d038f10d4b4b4ba63a8cf7a06658244d
Ahmed Mahgoub <aed7862@gmail.com>
2025-05-05 14:21:43 +0300

    refactor: delete handling exception in login use case

--- src/main/kotlin/logic/usecase/user/LoginUserUseCase.kt
+++ src/main/kotlin/logic/usecase/user/LoginUserUseCase.kt
@@ -5,20 +5,13 @@ import logic.model.User
 import logic.repositories.AuthenticationRepository
 
 class LoginUserUseCase(
-	private val authenticationRepository: AuthenticationRepository,
-	private val passwordEncryptor: PasswordEncryptor
+    private val authenticationRepository: AuthenticationRepository,
+    private val passwordEncryptor: PasswordEncryptor
 ) {
-	
-	fun login(name: String, password: String): User {
-		try {
-			val users = authenticationRepository.getAllUsers()
-			val hashedPassword = passwordEncryptor.hashPassword(password)
-			val user = users.find { it.name == name && it.password == hashedPassword }
-				?: throw Exception("Invalid username or password")
-			
-			return user
-		} catch (e: Exception) {
-			throw Exception("Error during login: ${e.message}")
-		}
-	}
+    fun login(name: String, password: String): User {
+        val users = authenticationRepository.getAllUsers()
+        val hashedPassword = passwordEncryptor.hashPassword(password)
+        return users.find { it.name == name && it.password == hashedPassword }
+            ?: throw Exception("Invalid username or password")
+    }
 }
