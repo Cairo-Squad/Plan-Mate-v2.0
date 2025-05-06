522057bd4b11386b11bcb0ada4a70e91e35507db
Ahmed Mahgoub <aed7862@gmail.com>
2025-05-06 18:34:48 +0300

    refactor: rename repository parameter for use case

--- src/main/kotlin/logic/usecase/state/CreateStateUseCase.kt
+++ src/main/kotlin/logic/usecase/state/CreateStateUseCase.kt
@@ -1,15 +1,13 @@
 package logic.usecase.state
 
 import data.repositories.mappers.toStateDto
-import data.repositories.mappers.toUserDto
 import logic.model.State
-import logic.model.User
 import logic.repositories.StatesRepository
 
 class CreateStateUseCase(
-    private val stateRepo: StatesRepository
+    private val stateRepository: StatesRepository
 ) {
     fun createState(state: State): Boolean {
-        return stateRepo.createState(state.toStateDto())
+        return stateRepository.createState(state.toStateDto())
     }
 }
