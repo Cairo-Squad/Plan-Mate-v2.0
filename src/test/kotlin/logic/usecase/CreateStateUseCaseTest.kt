fb8476817ce53077766794acdd8c4f563d637bb3
Ahmed Mahgoub <aed7862@gmail.com>
2025-05-06 18:35:05 +0300

    refactor: change test library to truth

--- src/test/kotlin/logic/usecase/CreateStateUseCaseTest.kt
+++ src/test/kotlin/logic/usecase/CreateStateUseCaseTest.kt
@@ -1,5 +1,6 @@
 package logic.usecase
 
+import com.google.common.truth.Truth.assertThat
 import data.dto.UserType
 import io.mockk.every
 import io.mockk.mockk
@@ -7,61 +8,60 @@ import logic.model.State
 import logic.model.User
 import logic.repositories.StatesRepository
 import logic.usecase.state.CreateStateUseCase
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.util.*
 
 class CreateStateUseCaseTest {
-	
-	private lateinit var statesRepository: StatesRepository
-	private lateinit var createStateUseCase: CreateStateUseCase
-	
-	@BeforeEach
-	fun setup() {
-		statesRepository = mockk()
-		createStateUseCase = CreateStateUseCase(statesRepository)
-	}
-	
-	@Test
-	fun `should return true when admin user creates state`() {
-		// Given
-		val state = State(UUID.randomUUID(), "Test State")
-		val user = User(UUID.randomUUID(), "admin", "pw", UserType.ADMIN)
-		
-		every { statesRepository.createState(any()) } returns true
-		
-		// When
-		val result = createStateUseCase.createState(state)
-		
-		// Then
-		assertTrue(result)
-	}
-	
-	@Test
-	fun `should return false when mate user tries to create state`() {
-		// Given
-		val state = State(UUID.randomUUID(), "Test State")
-		val user = User(UUID.randomUUID(), "mateUser", "pw", UserType.MATE)
-		
-		every { statesRepository.createState(any()) } returns false
-		
-		// When
-		val result = createStateUseCase.createState(state)
-		
-		// Then
-		assertFalse(result)
-	}
 
-	@Test
-	fun `should throw Exception when repository throws exception`() {
-		// Given
-		val state = State(UUID.randomUUID(), "Test State")
-		val user = User(UUID.randomUUID(), "mateUser", "pw", UserType.MATE)
-		every { statesRepository.createState(any(), any()) } throws Exception()
+    private lateinit var statesRepository: StatesRepository
+    private lateinit var createStateUseCase: CreateStateUseCase
 
-		// When & Then
-		assertThrows<Exception> { createStateUseCase.createState(state, user) }
-	}
+    @BeforeEach
+    fun setup() {
+        statesRepository = mockk()
+        createStateUseCase = CreateStateUseCase(statesRepository)
+    }
+
+    @Test
+    fun `should return true when admin user creates state`() {
+        // Given
+        val state = State(UUID.randomUUID(), "Test State")
+        val user = User(UUID.randomUUID(), "admin", "pw", UserType.ADMIN)
+
+        every { statesRepository.createState(any()) } returns true
+
+        // When
+        val result = createStateUseCase.createState(state)
+
+        // Then
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `should return false when mate user tries to create state`() {
+        // Given
+        val state = State(UUID.randomUUID(), "Test State")
+        val user = User(UUID.randomUUID(), "mateUser", "pw", UserType.MATE)
+
+        every { statesRepository.createState(any()) } returns false
+
+        // When
+        val result = createStateUseCase.createState(state)
+
+        // Then
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `should throw Exception when repository throws exception`() {
+        // Given
+        val state = State(UUID.randomUUID(), "Test State")
+        val user = User(UUID.randomUUID(), "mateUser", "pw", UserType.MATE)
+        every { statesRepository.createState(any()) } throws Exception()
+
+        // When & Then
+        assertThrows<Exception> { createStateUseCase.createState(state) }
+    }
 }
