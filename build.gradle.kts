c0d7b70107cfa0081caf4c6b482ae46479de9844
ahmed samy <ahmbdelkhalek@gmail.com>
2025-05-07 14:22:40 +0300

    refactor: add coroutine dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ dependencies {
     
     implementation("org.mongodb:mongodb-driver-sync:4.11.1")
     implementation("org.slf4j:slf4j-simple:2.0.12")
+    
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
 }
 
 kover {
