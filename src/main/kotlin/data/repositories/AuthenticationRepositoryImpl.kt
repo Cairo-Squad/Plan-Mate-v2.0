4f2e0885adda0318b6a989dc19efa487ed10416e
Ahmed Mahgoub <aed7862@gmail.com>
2025-05-10 21:20:08 +0300

    feat: add handling exception with base repository

--- src/main/kotlin/data/repositories/AuthenticationRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/AuthenticationRepositoryImpl.kt
@@ -5,43 +5,53 @@ import data.dto.UserType
 import data.hashing.PasswordEncryptor
 import data.repositories.mappers.toUser
 import data.repositories.mappers.toUserDto
-import logic.exception.UserNotFoundException
+import logic.exception.NotFoundException
 import logic.model.User
 import logic.repositories.AuthenticationRepository
-import java.util.UUID
+import java.util.*
 
 class AuthenticationRepositoryImpl(
     private val remoteDataSource: RemoteDataSource,
     private val passwordEncryptor: PasswordEncryptor
-) : AuthenticationRepository {
+) : AuthenticationRepository, BaseRepository() {
 
     override suspend fun getAllUsers(): List<User> {
-        val usersDto = remoteDataSource.getAllUsers()
-        return usersDto.map { it.toUser() }
+        return wrap {
+            val usersDto = remoteDataSource.getAllUsers()
+            usersDto.map { it.toUser() }
+        }
     }
 
     override suspend fun deleteUser(userId: UUID): Boolean {
-        val userDto = remoteDataSource.getAllUsers()
-            .find { it.id == userId } ?: throw UserNotFoundException()
-        remoteDataSource.deleteUser(userDto)
-        return true
+        return wrap {
+            val userDto = remoteDataSource.getAllUsers()
+                .find { it.id == userId } ?: throw NotFoundException()
+            remoteDataSource.deleteUser(userDto)
+            true
+        }
     }
 
-    override suspend fun createUser(id : UUID, name : String, password : String, userType : UserType) : Boolean {
-        val hashedPassword = passwordEncryptor.hashPassword(password)
-        return remoteDataSource.createUser(id, name, hashedPassword, userType)
+    override suspend fun createUser(id: UUID, name: String, password: String, userType: UserType): Boolean {
+        return wrap {
+            val hashedPassword = passwordEncryptor.hashPassword(password)
+            remoteDataSource.createUser(id, name, hashedPassword, userType)
+        }
     }
 
-    override suspend fun editUser(user: User){
-        return remoteDataSource.editUser(user.toUserDto())
+    override suspend fun editUser(user: User) {
+        return wrap { remoteDataSource.editUser(user.toUserDto()) }
     }
 
-    override suspend fun loginUser(name : String, password : String) :Boolean{
-        val hashedPassword = passwordEncryptor.hashPassword(password)
-        return remoteDataSource.loginUser(name,hashedPassword)
+    override suspend fun loginUser(name: String, password: String): Boolean {
+        return wrap {
+            val hashedPassword = passwordEncryptor.hashPassword(password)
+            remoteDataSource.loginUser(name, hashedPassword)
+        }
     }
 
     override suspend fun getCurrentUser(): User? {
-        return remoteDataSource.getCurrentUser()?.toUser()
+        return wrap {
+            remoteDataSource.getCurrentUser()?.toUser()
+        }
     }
 }
