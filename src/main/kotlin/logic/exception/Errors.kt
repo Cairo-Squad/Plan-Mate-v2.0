8214a0ea31df463fa5e6cdf9249f700db72d4881
Ahmed Mahgoub <aed7862@gmail.com>
2025-05-10 21:56:52 +0300

    refactor: reformat exceptions with hierarchy

--- src/main/kotlin/logic/exception/Errors.kt
+++ src/main/kotlin/logic/exception/Errors.kt
@@ -11,12 +11,6 @@ open class GeneralException : Exception()
 class NotFoundException : GeneralException()
 class UnknownException : GeneralException()
 
-class ProjectNotFoundException : Exception("Project not found")
-class WriteException : Exception("Failed to write to CSV file")
-class EmptyNameException : Exception("Name cannot be empty")
-class EmptyTitleException : Exception("Title cannot be empty")
+class EmptyNameException : IllegalArgumentException()
+class EmptyTitleException : IllegalArgumentException()
 class EntityNotChangedException : Exception("Entity not changed")
-class CsvParseException(message: String? = null, cause: Throwable? = null) :
-    Exception(message ?: "Failed to parse CSV data", cause)
-
-
