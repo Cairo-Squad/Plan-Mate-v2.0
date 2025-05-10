55a1e808329c3f0f102ccf048986e3b223e2e0af
Ahmed Mahgoub <aed7862@gmail.com>
2025-05-10 21:47:57 +0300

    refactor: improve format

--- src/main/kotlin/logic/exception/Errors.kt
+++ src/main/kotlin/logic/exception/Errors.kt
@@ -2,27 +2,20 @@ package logic.exception
 
 open class UserException : Exception()
 class InvalidUserCredentialsException : UserException()
-class UnauthorizedUserActionException : UserException()
+class InvalidUserException : UserException()
 
 open class AdminException : Exception()
+class UnauthorizedActionException : UserException()
 
 open class GeneralException : Exception()
 class NotFoundException : GeneralException()
-
-class UnknownException() : Exception("Unknown exception has occurred")
-
-class InvalidUserException : Exception("Mate is not allowed to create project")
+class UnknownException : GeneralException()
 
 class ProjectNotFoundException : Exception("Project not found")
-
 class WriteException : Exception("Failed to write to CSV file")
-
 class EmptyNameException : Exception("Name cannot be empty")
-
 class EmptyTitleException : Exception("Title cannot be empty")
-
 class EntityNotChangedException : Exception("Entity not changed")
-
 class CsvParseException(message: String? = null, cause: Throwable? = null) :
     Exception(message ?: "Failed to parse CSV data", cause)
 
