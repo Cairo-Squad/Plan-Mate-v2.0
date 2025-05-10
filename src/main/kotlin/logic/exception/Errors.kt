6a6d6590598500fba70a68a14cc5821d5ab98192
Ahmed Mahgoub <aed7862@gmail.com>
2025-05-10 22:22:54 +0300

    refactor: separate different exceptions to be readable

--- src/main/kotlin/logic/exception/Errors.kt
+++ src/main/kotlin/logic/exception/Errors.kt
@@ -11,6 +11,9 @@ open class GeneralException : Exception()
 class NotFoundException : GeneralException()
 class UnknownException : GeneralException()
 
+class EntityNotChangedException : Exception()
+
 class EmptyNameException : IllegalArgumentException()
 class EmptyTitleException : IllegalArgumentException()
-class EntityNotChangedException : Exception()
+
+
