78aebb39d891f9231820c2dfb17164b1e3c01da3
Ahmed Mahgoub <aed7862@gmail.com>
2025-05-10 21:22:01 +0300

    feat: add excpetion with hierarchies

--- src/main/kotlin/logic/exception/Errors.kt
+++ src/main/kotlin/logic/exception/Errors.kt
@@ -1,16 +1,19 @@
 package logic.exception
 
-import java.io.IOException
+open class UserException : Exception()
+class InvalidUserCredentialsException : UserException()
+class UnauthorizedUserActionException : UserException()
+
+open class AdminException : Exception()
+
+open class GeneralException : Exception()
+class NotFoundException : GeneralException()
 
 class UnknownException() : Exception("Unknown exception has occurred")
 
 class InvalidUserException : Exception("Mate is not allowed to create project")
 
-class UserNotFoundException: Exception("User not found")
-
 class ProjectNotFoundException : Exception("Project not found")
-
-class ReadException : IOException("Failed to read from this CSV file")
 
 class WriteException : Exception("Failed to write to CSV file")
 
@@ -18,13 +21,7 @@ class EmptyNameException : Exception("Name cannot be empty")
 
 class EmptyTitleException : Exception("Title cannot be empty")
 
-class EmptyPasswordException : Exception("Password cannot be empty")
-
 class EntityNotChangedException : Exception("Entity not changed")
-
-class DtoNotFoundException : Exception("Not Found DTO")
-
-class EntityNotFoundException() : Exception("Entity not found.")
 
 class CsvParseException(message: String? = null, cause: Throwable? = null) :
     Exception(message ?: "Failed to parse CSV data", cause)
