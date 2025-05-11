436f4f2a19e79b22fd49ae78b6817df089b3fa73
ahmed samy <ahmbdelkhalek@gmail.com>
2025-05-11 16:50:35 +0300

    refactor: remove commented out parts

--- src/main/kotlin/logic/usecase/task/CreateTaskUseCase.kt
+++ src/main/kotlin/logic/usecase/task/CreateTaskUseCase.kt
@@ -1,32 +1,14 @@
 package logic.usecase.task
 
-import logic.exception.EmptyTitleException
 import logic.model.Task
 import logic.repositories.TasksRepository
-import logic.usecase.log.AddLogUseCase
 
 class CreateTaskUseCase(
-    private val repository: TasksRepository, private val addLogUseCase: AddLogUseCase
+    private val repository: TasksRepository,
+    private val validationTask : ValidationTask
 ) {
     suspend fun createTask(task: Task) {
-        validateTask(task)
+        validationTask.validateCreateTask(task)
         repository.createTask(task)
-      /*  val log = Log(
-            id = UUID.randomUUID(),
-            entityId = task.id,
-            entityTitle = task.title,
-            entityType = EntityType.TASK,
-            dateTime = LocalDateTime.now(),
-            userId = getUser()!!.id,
-            userAction = UserAction.CreateTask(task.title, task.id, task.projectId)
-        )
-
-
-        addLogUseCase.addLog(log)*/
-    }
-
-    private fun validateTask(task: Task) {
-        if (task.title.isBlank()) throw EmptyTitleException()
     }
 }
-
