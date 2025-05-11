e175ba79c1b311596c12932cbb0b5ffd0154f966
VIP <gehadepada@gmail.com>
2025-05-11 14:28:58 +0300

    refactor : follow Given When Then approach in some test cases

--- src/test/kotlin/ui/features/user/admin/CreateNewUserViewTest.kt
+++ src/test/kotlin/ui/features/user/admin/CreateNewUserViewTest.kt
@@ -25,35 +25,43 @@ class CreateNewUserViewTest {
 
     @Test
     fun `should user see Username cannot be empty error when username is empty`() = runTest {
+        //Given
         every { inputHandler.promptForInput(any()) } returns ""
-
         createNewUserView.createNewUser()
 
+        //When
         verify { outputFormatter.printError("❌ Username cannot be empty.") }
         verify { inputHandler.waitForEnter() }
+
+        //Then
         coVerify(exactly = 0) { createUserUseCase.createUser(any()) }
     }
 
     @Test
     fun `should user see  Password cannot be empty error when password is empty`() = runTest {
+
+        //Given
         every { inputHandler.promptForInput(any()) } returns "nour"
         every { inputHandler.promptForPassword(any()) } returns ""
-
         createNewUserView.createNewUser()
 
+        //When
         verify { outputFormatter.printError("❌ Password cannot be empty.") }
         verify { inputHandler.waitForEnter() }
+
+        //Then
         coVerify(exactly = 0) { createUserUseCase.createUser(any()) }
     }
 
     @Test
     fun `should user see created  successfully  when created successfully in database `() = runTest {
+        //Given & When
         every { inputHandler.promptForInput(any()) } returns "nour"
         every { inputHandler.promptForPassword(any()) } returns "123456"
         coEvery { createUserUseCase.createUser(any()) } returns true
-
         createNewUserView.createNewUser()
 
+        //Then
         verify { outputFormatter.printSuccess("✅ User 'nour' created successfully!") }
         verify { inputHandler.waitForEnter() }
     }
