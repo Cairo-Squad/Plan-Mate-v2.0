60ebc486c431b52665e972f140e04eb2e1246d73
ahmed samy <ahmbdelkhalek@gmail.com>
2025-05-12 11:29:44 +0300

    refactor: make the id nullable

--- src/main/kotlin/data/dto/ProjectDto.kt
+++ src/main/kotlin/data/dto/ProjectDto.kt
@@ -3,7 +3,7 @@ package data.dto
 import java.util.UUID
 
 data class ProjectDto(
-    val id: UUID,
+    val id: UUID?,
     val title: String,
     val description: String,
     val createdBy: UUID,
