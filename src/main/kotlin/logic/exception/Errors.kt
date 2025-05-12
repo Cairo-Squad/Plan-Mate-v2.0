f3040644435ba1164e71bdbaefbc34b770291e3e
Mahmoud Khairy <mahmoud5airy2000@gmail.com>
2025-05-12 23:19:55 +0300

    feature : added NetworkException class to represent mongoDB exceptions.

--- src/main/kotlin/logic/exception/Errors.kt
+++ src/main/kotlin/logic/exception/Errors.kt
@@ -16,6 +16,13 @@ class EntityNotChangedException : Exception()
 class EmptyNameException : IllegalArgumentException()
 class EmptyTitleException : IllegalArgumentException()
 
-open class OperationException: Exception()
-class WriteException: OperationException()
+open class OperationException : Exception()
+class WriteException : OperationException()
+
+open class NetworkException : Exception() {
+    data class IOException(val errorMsg: String = "No Internet Connection") : NetworkException()
+    data class InvalidCollectionName(val errorMsg: String = "Invalid collection name") : NetworkException()
+    data class WriteException(val errorMsg: String? = "Api Write Exception") : NetworkException()
+    data class ApiException(val errorMsg: String? = "Api UnKnown Exception") : NetworkException()
+}
 
