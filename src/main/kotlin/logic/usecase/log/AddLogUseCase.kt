0441ad952d5069c0ee27e4ff6cd67a340ff6287b
Eman Abobakr <eman.abobakr@vodafone.com>
2025-05-13 07:43:27 +0300

    refactor: remove Result usage

--- src/main/kotlin/logic/usecase/log/AddLogUseCase.kt
+++ src/main/kotlin/logic/usecase/log/AddLogUseCase.kt
@@ -6,12 +6,7 @@ import logic.repositories.LogsRepository
 class AddLogUseCase(
     private val logsRepository: LogsRepository
 ) {
-    suspend fun addLog(log: Log): Result<Unit> {
-        return try {
-            logsRepository.addLog(log)
-            Result.success(Unit)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
+    suspend fun addLog(log: Log) {
+        logsRepository.addLog(log)
     }
 }
