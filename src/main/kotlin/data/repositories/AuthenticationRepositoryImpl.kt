1183aadcacde46f6ce5ef0379dfaf3eccec290eb
mohamed_Galal <az.galal666@gmail.com>
2025-05-13 15:12:44 +0300

    hotfix: remove unused try-catch in  AuthenticationRepositoryImpl

--- src/main/kotlin/data/repositories/AuthenticationRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/AuthenticationRepositoryImpl.kt
@@ -15,92 +15,45 @@ class AuthenticationRepositoryImpl(
 ) : AuthenticationRepository, BaseRepository() {
 
     override suspend fun getAllUsers(): List<User> {
-        return try {
-            wrap {
+        return wrap {
                 val usersDto = remoteDataSource.getAllUsers()
                 usersDto.map { it.toUser() }
             }
-        } catch (e: NotFoundException) {
-            throw NotFoundException()
-        } catch (e: GeneralException) {
-            throw GeneralException()
-        } catch (e: Exception) {
-            throw UnknownException()
-        }
     }
 
     override suspend fun deleteUser(userId: UUID): Boolean {
-        return try {
-            wrap {
+        return wrap {
                 val userDto = remoteDataSource.getAllUsers()
                     .find { it.id == userId } ?: throw NotFoundException()
                 remoteDataSource.deleteUser(userDto)
                 true
             }
-        } catch (e: NotFoundException) {
-            throw e
-        } catch (e: GeneralException) {
-            throw GeneralException()
-        } catch (e: Exception) {
-            throw UnknownException()
-        }
     }
 
     override suspend fun createUser(user: User): Boolean {
-        return try {
-            wrap {
-                remoteDataSource.createUser(user.toUserDto())
-            }
-        } catch (e: EntityNotChangedException) {
-            throw e
-        } catch (e: UserException) {
-            throw InvalidUserException()
-        } catch (e: Exception) {
-            throw UnknownException()
+        return wrap {
+            remoteDataSource.createUser(user.toUserDto())
         }
     }
 
     override suspend fun editUser(user: User): Boolean {
-        return try {
-            wrap { remoteDataSource.editUser(user.toUserDto()) }
-        } catch (e: EntityNotChangedException) {
-            throw e
-        } catch (e: UserException) {
-            throw InvalidUserException()
-        } catch (e: Exception) {
-            throw UnknownException()
-        }
+        return wrap { remoteDataSource.editUser(user.toUserDto()) }
+
     }
 
     override suspend fun loginUser(name: String, password: String): Boolean {
-        return try {
-            wrap {
-                val hashedPassword = passwordEncryptor.hashPassword(password)
-                if (!remoteDataSource.loginUser(name, hashedPassword)) {
-                    throw InvalidUserCredentialsException()
-                }
-                true
+        return wrap {
+            val hashedPassword = passwordEncryptor.hashPassword(password)
+            if (!remoteDataSource.loginUser(name, hashedPassword)) {
+                throw InvalidUserCredentialsException()
             }
-        } catch (e: InvalidUserCredentialsException) {
-            throw e
-        } catch (e: UserException) {
-            throw InvalidUserException()
-        } catch (e: Exception) {
-            throw UnknownException()
+            true
         }
     }
 
     override suspend fun getCurrentUser(): User? {
-        return try {
-            wrap {
-                remoteDataSource.getCurrentUser()?.toUser()
-            }
-        } catch (e: NotFoundException) {
-            throw e
-        } catch (e: GeneralException) {
-            throw GeneralException()
-        } catch (e: Exception) {
-            throw UnknownException()
+        return wrap {
+            remoteDataSource.getCurrentUser()?.toUser()
         }
     }
 }
