5eae31d12a74bb641922e0f39d12909c0e848cb3
norhanadel367 <norhanadel367@gmail.com>
2025-05-15 12:16:44 +0300

    test:  add overloading fun

--- src/main/kotlin/data/dataSource/remoteDataSource/mongo/handler/MongoDBHandlerImpl.kt
+++ src/main/kotlin/data/dataSource/remoteDataSource/mongo/handler/MongoDBHandlerImpl.kt
@@ -9,12 +9,12 @@ import org.bson.Document
 import java.util.*
 
 abstract class MongoDBHandlerImpl<DTO>(
-    protected val database: MongoDatabase,
-    protected val collectionName: String,
-    protected val getDtoId: (DTO) -> UUID
+    protected val database : MongoDatabase,
+    protected val collectionName : String,
+    protected val getDtoId : (DTO) -> UUID
 ) : MongoDBHandler<DTO> {
 
-    private val collection: MongoCollection<Document> by lazy {
+    private val collection : MongoCollection<Document> by lazy {
         database.getCollection(collectionName)
     }
 
@@ -24,40 +24,57 @@ abstract class MongoDBHandlerImpl<DTO>(
         }
     }
 
-    abstract fun convertDtoToDocument(entity: DTO): Document
-    abstract fun convertDocumentToDto(document: Document): DTO
+    abstract fun convertDtoToDocument(entity : DTO) : Document
+    abstract fun convertDocumentToDto(document : Document) : DTO
 
-    private fun collectionExists(): Boolean {
+    private fun collectionExists() : Boolean {
         return database.listCollectionNames().contains(collectionName)
     }
 
-    override fun write(entity: DTO): Boolean {
+    override fun write(entity : DTO) : Boolean {
         val document = convertDtoToDocument(entity)
-        val result: InsertOneResult = collection.insertOne(document)
+        val result : InsertOneResult = collection.insertOne(document)
         return result.wasAcknowledged()
     }
 
-    override fun edit(entity: DTO): Boolean {
+    override fun write(entity : DTO, fakeParam : String) : DTO {
+        val document = convertDtoToDocument(entity)
+        collection.insertOne(document)
+        return convertDocumentToDto(document)
+    }
+
+    override fun edit(entity : DTO) : Boolean {
         val entityId = getDtoId(entity)
         val document = convertDtoToDocument(entity)
         val result = collection.replaceOne(Filters.eq("_id", entityId.toString()), document)
         return result.wasAcknowledged()
     }
 
-    override fun delete(entityId: UUID): Boolean {
+    override fun delete(entityId : UUID) : Boolean {
         val result = collection.deleteOne(Filters.eq("_id", entityId.toString()))
         return result.wasAcknowledged()
     }
 
-    override fun readAll(): List<DTO> {
+    override fun delete(entityId : UUID, fakeParam : String) {
+        collection.deleteOne(Filters.eq("_id", entityId.toString()))
+    }
+
+    override fun edit(entity : DTO, fakeParam : String) {
+        val entityId = getDtoId(entity)
+        val document = convertDtoToDocument(entity)
+        collection.replaceOne(Filters.eq("_id", entityId.toString()), document)
+    }
+
+    override fun readAll() : List<DTO> {
         return collection.find()
             .map { convertDocumentToDto(it) }
             .toList()
     }
 
-    override fun readByEntityId(id: UUID): DTO {
+    override fun readByEntityId(id : UUID) : DTO {
         val document = collection.find(Filters.eq("_id", id.toString())).first()
             ?: throw PlanMateException.NetworkException.DataNotFoundException()
         return convertDocumentToDto(document)
     }
+
 }
